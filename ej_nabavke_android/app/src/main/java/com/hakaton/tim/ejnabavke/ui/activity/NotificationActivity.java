d652b8b7c7879ee730140dc343fdb72598a3c5cb
Bojan Kopanja <kopanja@gmail.com>
2015-12-06 11:20:58 +0100

    Notifikacije menuoptions

--- ej_nabavke_android/app/src/main/java/com/hakaton/tim/ejnabavke/ui/activity/NotificationActivity.java
+++ ej_nabavke_android/app/src/main/java/com/hakaton/tim/ejnabavke/ui/activity/NotificationActivity.java
@@ -1,10 +1,13 @@
 package com.hakaton.tim.ejnabavke.ui.activity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 
@@ -56,5 +59,23 @@ public class NotificationActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_filter, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionNext:
+                Intent next = new Intent(this, FeedActivity.class);
+                startActivity(next);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
